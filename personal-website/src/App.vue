1920826c647f8a1cf904fcc884d1952add6e7fc5
mqsy2 <mqsy2@student.apc.edu.ph>
2025-03-06 10:10:13 +0000

    something

--- personal-website/src/App.vue
+++ personal-website/src/App.vue
@@ -6,8 +6,8 @@ import Guestbook from './components/Guestbook.vue'
   <header>
     <nav>
       <ul>
-        <li>Home</li>
-        <li>About Me</li>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About Me</router-link></li>
       </ul>
     </nav>
   </header>
@@ -21,26 +21,4 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
