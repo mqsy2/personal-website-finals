f3cdd3e9515ae53be4047fb7eb7bcf31b7a9ee93
mqsy2 <mqsy2@student.apc.edu.ph>
2025-03-06 13:02:25 +0000

    fafa

--- personal-website/src/App.vue
+++ personal-website/src/App.vue
@@ -4,29 +4,11 @@ import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <div class="container">
-    <Navbar />
-    <main>
-      <router-view />
-      <Guestbook />
-    </main>
-  </div>
+  <Navbar />
+  <router-view />
+  <Guestbook />
 </template>
 
-<style>
-/* Center everything */
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center; /* Center horizontally */
-  justify-content: center; /* Center vertically if needed */
-  min-height: 100vh; /* Full height */
-  text-align: center; /* Ensure text is centered */
-}
+<style scoped>
 
-main {
-  max-width: 800px; /* Keep content readable */
-  width: 100%;
-  padding: 20px;
-}
 </style>
