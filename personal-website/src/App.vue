3798407a3aa48cd5c99e3ac3930ce68c11a71a66
mqsy2 <mqsy2@student.apc.edu.ph>
2025-03-06 13:21:50 +0000

    gfafaf

--- personal-website/src/App.vue
+++ personal-website/src/App.vue
@@ -4,33 +4,15 @@ import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <div class="app-container">
+
     <Navbar />
     <router-view />
     <div class="guestbook-container">
       <Guestbook />
     </div>
-  </div>
+    
 </template>
 
 <style>
-/* Dark background for the whole website */
-.app-container {
-  background-color: #121212; /* Dark gray/black */
-  color: #ffffff; /* Light text for contrast */
-  min-height: 100vh; /* Ensures full height */
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
 
-/* Center Guestbook */
-.guestbook-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  margin-top: 20px;
-}
 </style>
