e3c78cfd6df6cb52fd7f4fe185e76e93d543cd56
mqsy2 <mqsy2@student.apc.edu.ph>
2025-03-06 10:46:28 +0000

    f

--- personal-website/src/router/index.js
+++ personal-website/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../view/home.vue';
-import About from '../view/about.vue';
+import About from '../view/aboutus.vue';
 
 const routes = [
   { path: '/', component: Home },
