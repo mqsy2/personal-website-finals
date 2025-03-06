14e2fac1db199c3ac373d49f4eeed252eb8a7b1b
mqsy2 <mqsy2@student.apc.edu.ph>
2025-03-06 10:42:56 +0000

    r

--- personal-website/src/main.js
+++ personal-website/src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './route'
+import router from './router'
 
 const app = createApp(App)
 app.use(router)
