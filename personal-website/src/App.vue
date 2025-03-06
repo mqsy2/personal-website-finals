8b9ca303f37090e00094a25458baf9d9a06dbe8d
mqsy2 <mqsy2@student.apc.edu.ph>
2025-03-06 13:05:07 +0000

    fafa

--- personal-website/src/App.vue
+++ personal-website/src/App.vue
@@ -6,9 +6,17 @@ import Navbar from './components/Navbar.vue';
 <template>
   <Navbar />
   <router-view />
-  <Guestbook />
+  <div class="guestbook-container">
+    <Guestbook />
+  </div>
 </template>
 
 <style scoped>
-
+.guestbook-container {
+  display: flex;
+  justify-content: center; /* Centers horizontally */
+  align-items: center; /* Centers vertically if needed */
+  width: 100%;
+  margin-top: 20px; /* Adjust spacing */
+}
 </style>
